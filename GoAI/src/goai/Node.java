14f371ed0cd1629cc341300ea447e50d9d36be63
Jonii <joni.hanski@helsinki.fi>
2016-02-01 22:28:00 +0200

    Projektin runkoa + ekat testien rungot

---
+++ GoAI/src/goai/Node.java
@@ -0,0 +1,31 @@
+/** 
+ * Hakupuun solmu.
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package goai;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author jphanski
+ */
+public class Node {
+    LinkedList<Node> children;
+    
+    Pelilauta lauta;
+    
+    public void selectAction() { 
+        return;
+    }
+    
+    public void expand() {
+        return;
+    }
+    
+    public void simulate() {
+        return;
+    }
+    
+}
